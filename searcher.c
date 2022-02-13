7e94307f951f977dc6538033e914eb52e3a712d8
marzie400 <95773612+marzie400@users.noreply.github.com>
2022-02-13 08:35:05 -0800

    Add files via upload

--- searcher.c
+++ searcher.c
@@ -1,33 +1,40 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-char set_output(char *str){
-           printf("word is %s success\n",str);
+
+
+
+
+  int i=0;
+char set_output(char str[i]){
+           printf("*******word is %s success\n",&str[i]);
                  }
 char main(){
   FILE *fp;
-  char str;
-  char temp[1000];
+  int i=0;
+  char str[i];
+  char temp[100000];
 
-    if((fp = fopen("test.txt", "r")) != NULL) {
+    while ((fp = fopen("test.txt", "r")) != NULL) {
+    
+      if (fp){
 		printf("oh! test.txt found!\n");
 	}
                 (fgets(temp, 1000, fp) != NULL) ;
          
             printf("What word are you looking for? please write:\n");
-            scanf("%s",&str);
+            scanf("%s",&str[i]);
             printf("OK. I'm getting to work now ...\n");
-            if ((strstr(temp, &str)) == NULL) {
-            
-            printf("sorry! not found\n");
+            if ((strstr(temp, &str[i])) == NULL) {
+                        printf("sorry! not found\n");
                       }
-             else {
-                      set_output(&str);
-               }
+                       else {
+                      set_output(&str[i]);
+                        //fp = freopen("test.txt", "w+", stdout);
+                             }
                          fclose(fp);
 
                printf("test.txt removed!\n");
-                   		
-    printf("Now I'm waiting for test.txt again...\n");                           
+               printf("Now I'm waiting for test.txt again...\n");                           
   }
-
+}
